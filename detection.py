acafbf27675823d255c49b5c5b6584b55d106ef7
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2024-02-11 19:49:29 -0500

    Update detection.py

--- detection.py
+++ detection.py
@@ -13,17 +13,19 @@ def detect_lines(frame):
     y_end = int(height * 0.7)
 
     # Create a mask of zeros
-    #https://numpy.org/doc/stable/reference/generated/numpy.ma.make_mask.html
+    #https://numpy.org/doc/stable/reference/generated/numpy.ma.make_mask.html Line 15
     mask = np.zeros(frame.shape[:2], dtype=np.uint8)
 
     # Define the ROI (Region of Interest) on the mask
-    #https://stackoverflow.com/questions/15424852/region-of-interest-opencv-python
+    #https://stackoverflow.com/questions/15424852/region-of-interest-opencv-python Line 2
     mask[y_start:y_end, x_start:x_end] = 255
 
     # Convert to grayscale
-    #https://www.quora.com/How-do-you-convert-an-RGB-image-to-grayscale-with-OpenCV-and-or-Python
+    #https://www.quora.com/How-do-you-convert-an-RGB-image-to-grayscale-with-OpenCV-and-or-Python Line 4
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    
+    # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Lines 35-38
+    # https://pyimagesearch.com/2021/01/19/image-masking-with-opencv/ Lines 20-26
     # Apply the mask to the grayscale image
     masked_gray = cv2.bitwise_and(gray, gray, mask=mask)
 
@@ -36,6 +38,6 @@ def detect_lines(frame):
     edges = cv2.Canny(blurred, 50, 150, apertureSize=3)
 
     # Use HoughLinesP to detect lines within the masked area
-    #https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+    # https://github.com/adityagandhamal/road-lane-detection/blob/master/detection_on_vid.py Line 42 
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=80, minLineLength=100, maxLineGap=200)
     return lines
